1a033df4c5600bb95f771c834b02095ee79b6f10
Jerzy Chalupski <jerzy@getbase.com>
2015-08-25 16:01:24 +0200

    ClassScanner - MicroOrmDetector

--- src/main/java/com/getbase/lint/CustomIssueRegistry.java
+++ src/main/java/com/getbase/lint/CustomIssueRegistry.java
@@ -3,6 +3,7 @@ package com.getbase.lint;
 import com.android.tools.lint.client.api.IssueRegistry;
 import com.android.tools.lint.detector.api.Issue;
 import com.getbase.lint.issues.AncientMinSdkDetector;
+import com.getbase.lint.issues.MicroOrmDetector;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +11,9 @@ import java.util.List;
 public class CustomIssueRegistry extends IssueRegistry {
   @Override
   public List<Issue> getIssues() {
-    return Arrays.asList(AncientMinSdkDetector.ISSUE);
+    return Arrays.asList(
+        AncientMinSdkDetector.ISSUE,
+        MicroOrmDetector.NO_DEFAULT_CONSTRUCTOR_ISSUE
+    );
   }
 }
---
+++ src/main/java/com/getbase/lint/issues/MicroOrmDetector.java
@@ -0,0 +1,91 @@
+package com.getbase.lint.issues;
+
+import static com.android.SdkConstants.CONSTRUCTOR_NAME;
+
+import com.android.tools.lint.detector.api.Category;
+import com.android.tools.lint.detector.api.ClassContext;
+import com.android.tools.lint.detector.api.Detector;
+import com.android.tools.lint.detector.api.Detector.ClassScanner;
+import com.android.tools.lint.detector.api.Implementation;
+import com.android.tools.lint.detector.api.Issue;
+import com.android.tools.lint.detector.api.LintUtils;
+import com.android.tools.lint.detector.api.Scope;
+import com.android.tools.lint.detector.api.Severity;
+
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AnnotationNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.MethodNode;
+
+import java.util.List;
+
+public class MicroOrmDetector extends Detector implements ClassScanner {
+
+  public static final Issue NO_DEFAULT_CONSTRUCTOR_ISSUE = Issue.create(
+      "MicroOrmDefaultConstructor",
+      "MicroOrm annotated class without default constructor",
+      "`MicroOrm.fromCursor()` and `MicroOrm.getFunctionFor()` needs a default constructor.",
+      Category.CORRECTNESS,
+      7,
+      Severity.WARNING,
+      new Implementation(MicroOrmDetector.class, Scope.CLASS_FILE_SCOPE));
+
+  @Override
+  public void checkClass(ClassContext context, ClassNode classNode) {
+    super.checkClass(context, classNode);
+
+    if (!isInstantiable(classNode) && isAnnotatedByMicroOrm(classNode)) {
+
+      context.report(
+          NO_DEFAULT_CONSTRUCTOR_ISSUE,
+          context.getLocation(classNode),
+          String.format(
+              "This class should provide a default constructor (a constructor with no arguments) (%1$s)",
+              ClassContext.createSignature(classNode.name, null, null)
+          )
+      );
+    }
+  }
+
+  private static final String COLUMN_ANNOTATION_DESC = "Lorg/chalup/microorm/annotations/Column;";
+  private static final String EMBEDDED_ANNOTATION_DESC = "Lorg/chalup/microorm/annotations/Embedded;";
+
+  private static boolean isAnnotatedByMicroOrm(ClassNode classNode) {
+    for (Object f : classNode.fields) {
+      FieldNode field = (FieldNode) f;
+      if (field.visibleAnnotations != null) {
+        for (Object a : field.visibleAnnotations) {
+          AnnotationNode annotationNode = (AnnotationNode) a;
+          if (COLUMN_ANNOTATION_DESC.equals(annotationNode.desc) || EMBEDDED_ANNOTATION_DESC.equals(annotationNode.desc)) {
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+  private static boolean isInstantiable(ClassNode classNode) {
+    if ((classNode.access & Opcodes.ACC_PUBLIC) == 0) {
+      return false;
+    }
+
+    if (classNode.name.indexOf('$') != -1 && !LintUtils.isStaticInnerClass(classNode)) {
+      return false;
+    }
+
+    @SuppressWarnings("rawtypes")
+    List methodList = classNode.methods;
+    for (Object m : methodList) {
+      MethodNode method = (MethodNode) m;
+      if (method.name.equals(CONSTRUCTOR_NAME)) {
+        if (method.desc.equals("()V")) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
